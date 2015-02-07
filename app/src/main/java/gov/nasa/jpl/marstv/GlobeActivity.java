6e28cbeaf9f9e5d3b32bd859a879e232fd73010f
chadmart@usc.edu <mrComputer@guest-wireless-207-151-035-010.usc.edu>
2015-02-07 13:39:56 -0800

    openGL code

--- app/src/main/java/gov/nasa/jpl/marstv/GlobeActivity.java
+++ app/src/main/java/gov/nasa/jpl/marstv/GlobeActivity.java
@@ -19,7 +19,6 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
-import android.os.Bundle;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -34,11 +33,9 @@ public class GlobeActivity extends Activity {
     /** Hold a reference to our GLSurfaceView */
     private GLSurfaceView mGLSurfaceView;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
+    @AfterViews
+    void init() {
+        System.out.println("hello tv");
         mGLSurfaceView = new GLSurfaceView(this);
 
         // Check if the system supports OpenGL ES 2.0.
@@ -46,8 +43,11 @@ public class GlobeActivity extends Activity {
         final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
         final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
 
-        if (supportsEs2)
+        System.out.println(configurationInfo.reqGlEsVersion);
+//        if (supportsEs2)
+        if (true)
         {
+            System.out.println("hello tv2");
             // Request an OpenGL ES 2.0 compatible context.
             mGLSurfaceView.setEGLContextClientVersion(2);
 
@@ -56,12 +56,13 @@ public class GlobeActivity extends Activity {
         }
         else
         {
+
+            System.out.println("OO NOOO");
             // This is where you could create an OpenGL ES 1.x compatible
             // renderer if you wanted to support both ES 1 and ES 2.
             return;
         }
 
-        setContentView(mGLSurfaceView);
     }
 
     @Override
@@ -70,6 +71,7 @@ public class GlobeActivity extends Activity {
         // The activity must call the GL surface view's onResume() on activity onResume().
         super.onResume();
         mGLSurfaceView.onResume();
+
     }
 
     @Override
